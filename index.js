a12c0644ad75460bceddd48fa96f50d78af79ee4
Jaeho <lee.jaeho8023@gmail.com>
2018-12-06 14:27:48 +0900

    hmc_simulator start

---
+++ index.js
@@ -0,0 +1,39 @@
+// Express Loading
+const express = require('express');
+const app = express();
+
+// MySQL setting
+// Definition
+const mysql = require('mysql');
+const conn_db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'spp',
+  password : 'aleldjwps',
+  database : 'hmc_chatbot'
+});
+
+// Connection
+conn_db.connect();
+
+// BodyParser Loading
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setting for using jade
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+// Location
+app.listen(23705, function(){
+	console.log('Connected, 23705 port!');
+});
+
+// View page
+app.get('/view', function(req, res) {
+	var sql1 = "SELECT * from TB_USER_INPUT"
+	conn_db.query(sql1, function(err, user_inputs, body) {
+		console.log(user_inputs);
+	});
+	res.render('view')
+});
