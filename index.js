120a44d4affe0c37e2e4cc6c9308de21ed55dc23
Jaeho <lee.jaeho8023@gmail.com>
2018-12-07 15:39:23 +0900

    insert into DB added

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ app.get('/view', function(req, res) {
 app.get('/mode', function(req, res) {
 	var sql = "SELECT * FROM tb_utterance_manage"
 	conn_db.query(sql, function(err, manage_table, body) {
+		console.log('%%% Server log: /mode ROUTER');
+
 		console.log("Intention: " + manage_table[0].intention);
 		console.log("User Input: " + manage_table[0].user_input);
 		console.log("Response text: " + manage_table[0].response_text);
@@ -62,11 +64,17 @@ app.post('/input', function(req, res) {
 	var intention = req.body.selectedIntention;
 	var new_userInput = req.body.new_userInput;
 	var new_responseText = req.body.new_responseText;
+	var new_chatbotStatus = req.body.new_chatbotStatus;
 
 	console.log("%%% Server log: /input ROUTER");
 	console.log("Intention: " + intention);
 	console.log("New Input: " + new_userInput);
 	console.log("New Response: " + new_responseText);
+	console.log("New Status: " + new_chatbotStatus);
 
-	res.redirect('/mode')
+	var sql = 'INSERT INTO tb_utterance_manage (intention, user_input, response_text, chatbot_status) VALUES(?, ?, ?, ?)';
+	conn.query(sql, [intention, new_userInput, new_responseText, new_chatbotStatus], function(err, result, fields){
+		console.log("%%% Server log: New information Successfully added in DB.");
+		res.redirect('/mode')
+	});
 });
